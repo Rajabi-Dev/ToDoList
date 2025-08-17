2d679a2c1ef9e01fe8b4a656c39efbc582b73cb7
mahdi <mohammadmahdirajabi.net@gmail.com>
2025-08-17 19:44:11 +0330

    Rename:public to docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'public/scripts'),
+        path: path.resolve(__dirname, 'docs/scripts'),
         filename: 'bundle.js'
     },
 
     // devServer: {
-    //     contentBase: path.resolve(__dirname, 'public'),
+    //     contentBase: path.resolve(__dirname, 'docs'),
     //     publicPath:'/scripts/',
     // }
 };
